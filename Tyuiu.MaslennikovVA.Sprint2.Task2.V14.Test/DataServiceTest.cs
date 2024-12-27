4b47c4303e06002919dc604d6f37c82d5ef89e94
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 20:05:25 +0500

    Task2

---
+++ Tyuiu.MaslennikovVA.Sprint2.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.MaslennikovVA.Sprint2.Task2.V14.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint2.Task2.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 4;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(res, true);
+        }
+    }
+}
